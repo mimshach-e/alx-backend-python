7fb7ff3d7459b8cbe6283b4f3d36d1c6feb557c7
Ernest Mimshach <mimshach.e@gmail.com>
2025-09-20 15:18:50 +0000

    added default authentication settings for restframework

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -69,6 +69,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'messaging_app.wsgi.application'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
